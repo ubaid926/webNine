acdcbd5a2aab379f78ef2276a3d8e409a5a4384d
ubaid926 <s.ghani12390@gmail.com>
2026-06-16 02:24:12 +0500

    hero section

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import HeroSection from './components/heroSection'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <HeroSection />
+    </>
+  )
+}
+
+export default App
